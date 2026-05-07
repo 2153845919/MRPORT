46f563bd35727396a74907219bcf5471330d1b5f
root <root@localhost.localdomain>
2026-05-07 08:09:11 +0800

    Fix WinDivert download URL (2.2.2-A.zip)

--- download-windivert.sh
+++ download-windivert.sh
@@ -2,16 +2,18 @@
 # download-windivert.sh - Download WinDivert DLLs for build
 set -e
 
-WINDIVERT_VERSION="2.2.0"
+WINDIVERT_VERSION="2.2.2"
+ARCH="A"  # A=x64, B=ARM64, C=x86
 BASE_URL="https://github.com/basil00/WinDivert/releases/download/v${WINDIVERT_VERSION}"
 
+ZIP_NAME="WinDivert-${WINDIVERT_VERSION}-${ARCH}.zip"
 echo "Downloading WinDivert v${WINDIVERT_VERSION}..."
-curl -sL "${BASE_URL}/WinDivert-${WINDIVERT_VERSION}-x64.zip" -o /tmp/windivert.zip
+curl -sL "${BASE_URL}/${ZIP_NAME}" -o /tmp/windivert.zip
 unzip -o -q /tmp/windivert.zip -d /tmp/windivert-extract
 
 mkdir -p WinDivert
-cp /tmp/windivert-extract/x64/WinDivert64.sys WinDivert/
-cp /tmp/windivert-extract/x64/WinDivert.dll WinDivert/
+cp /tmp/windivert-extract/WinDivert-${WINDIVERT_VERSION}-${ARCH}/x64/WinDivert64.sys WinDivert/
+cp /tmp/windivert-extract/WinDivert-${WINDIVERT_VERSION}-${ARCH}/x64/WinDivert.dll WinDivert/
 
 echo "WinDivert files extracted to WinDivert/"
 ls -la WinDivert/
